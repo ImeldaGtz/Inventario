e06f72157ff32dac347dc0838796e0ca57952d80
Imelda Gtz <igutierrez22@ucol.mx>
2025-10-12 01:03:40 -0600

    Corrección de _encontrarParaAgregar, revisado

--- Inventario_Ordenado/inventario.js
+++ Inventario_Ordenado/inventario.js
@@ -3,10 +3,23 @@ export class Inventario{
         this.productos = [];
     }
     agregar(producto){
+        console.log('Entra a la función');
+        console.log(producto);
         if (this._codigoEnExistencia(producto.codigo)) {
+            console.log('Ya existe el codigo');
             return false; // 'Codigo ya existente'; // No se pudo
         } else {
-            let posición = this._encontrarParaAgregar(producto.codigo);
+            let posicion = this._encontrarParaAgregar(producto.codigo);
+            if (posicion == this.productos.length){
+                this.productos.push(producto);
+            } else{
+            console.log('Acomodanding');
+                this.productos.push();
+                for(let i = this.productos.length-1; i> posicion+1; i++) {
+                    this.productos[i] = this.productos[i-1];
+                }
+                this.productos[posicion] = producto;
+            }
             return true; // 'Producto agregado'; // Se pudo
         }
     }
@@ -19,12 +32,14 @@ export class Inventario{
         return info;
     }
     buscar(codigo){
+        // let resultado = this.productos[this._buscarIndice(codigo)];
         let resultado;
         for(let i = 0; i< this.productos.length; i++) {
             if(this.productos[i].codigo == codigo) {
                 resultado = this.productos[i];
             }
         }
+        return resultado == undefined ? null : resultado;
         return resultado == undefined ? null : resultado;
     }
 
@@ -52,7 +67,7 @@ export class Inventario{
         return resultado;
     }
 
-
+    //Por revisar
     _buscarIndice(codigo) {
         let resultado;
         if(this.productos.length == 1) {
@@ -78,29 +93,34 @@ export class Inventario{
     _codigoEnExistencia(codigo){
         return this.buscar(codigo) == null ? false : true;
     }
-    
+    // Por revisar
     _encontrarParaAgregar(codigo) {
+        console.log('Entra a la función');
         let resultado;
-        if (this.productos[0].codigo < codigo) {
-            resultado = 0;
-        } else if(this.productos[this.productos.length-1].codigo < codigo) {
-            resultado = this.productos.length;
+        if(this.productos.length == 0) {
+            return 0;
         } else {
-            let i = 0;
-            let f = this.productos.length-1;
-            let m;
-            while(i != m) {
-                m = Math.floor((i + f) / 2);
-                if(this.productos[m].codigo == codigo){
-                    resultado = m+1; //?
+            if (this.productos[0].codigo < codigo) {
+                resultado = 0;
+            } else if(this.productos[this.productos.length-1].codigo < codigo) {
+                resultado = this.productos.length;
+            } else {
+                let i = 0;
+                let f = this.productos.length-1;
+                let m;
+                while(i != m) {
+                    m = Math.floor((i + f) / 2);
+                    if(this.productos[m].codigo == codigo){
+                        resultado = m+1; //?
+                    }
+                    else if(this.productos[m].codigo > codigo) {
+                        f = m;
+                    } else {
+                        i = m;
+                    }
                 }
-                else if(this.productos[m].codigo > codigo) {
-                    f = m;
-                } else {
-                    i = m;
-                }
+                resultado = m+1;
             }
-            resultado = m+1;
         }
        return resultado;
 
